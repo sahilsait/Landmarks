f83219a55612d773ce57f8902ced349828254d80
sahilsait <sahilsaitdev@gmail.com>
2024-01-13 10:27:41 +0530

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Sahil SAIT on 12/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapVIew()
+                .frame(height: 300)
+            
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+        
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sahil SAIT on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
